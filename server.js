91c95d7d166b2be662a19774f40d9f6a44e7c0d5
Chase L <chase.lane24@gmail.com>
2023-01-26 19:20:58 -0600

    passport intro

---
+++ server.js
@@ -0,0 +1,114 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const PORT = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const initializePassport = require('./passport-config')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const sequelize = require('./config/connection');
+const User = require('./models/User');
+// sql creation code here
+// const user = require('./models/user')
+// function for finding user based on email and configuring the passport
+// initializePassport(
+//   passport,
+//   email => User.findOne({email: email}),
+//   id => User.findOne({_id: id})
+// )
+
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+// temporary variable to store users 
+const users= [] 
+// take the forms and access them inside of request variable in the post method
+// allows use of ejs
+app.set('view-engine','ejs')
+app.use(express.urlencoded({extended:false}))
+app.use(flash())
+app.use(session({
+    // .env
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+// what we pass for our method
+app.use(methodOverride('_method'))
+
+// route for index
+app.get('/', checkAuthenticated, (req,res) => {
+    res.render('index.ejs', { name: req.user.name })
+})
+
+// route for login
+app.get('/login', checkNotAuthenticated, (req,res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}) )
+
+// route for register
+app.get('/register', checkNotAuthenticated, (req,res) => {
+    res.render('register.ejs')
+})
+               
+app.post('/register', checkNotAuthenticated, async (req,res) => {
+// creating hashed password
+    try {
+         const hashedPassword = await bcrypt.hash(req.body.password, 10)
+         
+         //const user = await User.create({
+            users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+         })
+         res.redirect('/login')
+
+    } catch {
+        res.redirect('/register')
+    }
+     console.log(users)
+})
+
+app.delete('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/login');
+    });
+  });
+
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated (req, res, next) {
+    if (req.isAuthenticated()) {
+       return res.redirect('/')
+    }
+
+    next()
+}
+
+
+app.listen(PORT)
