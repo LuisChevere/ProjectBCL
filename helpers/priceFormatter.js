4cabb73e1c10f01c969ace9e8562386ae5f24553
Bonee94 <bonee94@yahoo.com>
2023-01-26 22:39:44 -0600

    Created a helper function to format a passed decimal interger and return as a monetary string

---
+++ helpers/priceFormatter.js
@@ -0,0 +1,15 @@
+//Helper function that accepts a decimal value and returns a monetary formatted string representation
+
+const priceFormat = (int) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  return formatter.format(int);
+};
+
+//console.log(priceFormat(793322.23)); returns $793,322.23
+
+
+//module.exports = priceFormat;
