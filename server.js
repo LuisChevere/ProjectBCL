094b8ea8dac0c556f234350ca9323eb91f4f5c4a
Bonee94 <bonee94@yahoo.com>
2023-02-02 16:03:54 -0600

    created authentication functions in helpers for middleware.

--- server.js
+++ server.js
@@ -1,60 +1,71 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const path = require('path');
+const path = require("path");
 const PORT = process.env.PORT || 3000;
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const initializePassport = require('./passport-config')
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
-const sequelize = require('./config/connection');
-const User = require('./models/User');
-const routes = require('./controllers');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const initializePassport = require("./passport-config");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const sequelize = require("./config/connection");
+const User = require("./models/User");
+const routes = require("./controllers");
+const exphbs = require("express-handlebars");
+const {
+  checkAuthenticated,
+  checkNotAuthenticated,
+  priceFormat,
+} = require("./helpers/index");
+
 // sql creation code here
 // const user = require('./models/user')
 // function for finding user based on email and configuring the passport
 initializePassport(
   passport,
-  email => User.findOne({email: email}),
-  id => User.findOne({_id: id})
-)
+  (email) => User.findOne({ email: email }),
+  (id) => User.findOne({ _id: id })
+);
 
-const hbs = exphbs.create();
+const hbs = exphbs.create({
+  checkAuthenticated,
+  checkNotAuthenticated,
+  priceFormat,
+});
 
 // initializePassport(
 //   passport,
 //   email => users.find(user => user.email === email),
 //   id => users.find(user => user.id === id)
 // )
-// temporary variable to store users 
-const users= [] 
+// temporary variable to store users
 // take the forms and access them inside of request variable in the post method
 // allows use of ejs
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-app.use(express.urlencoded({extended:false}))
-app.use(flash())
-app.use(session({
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
     // .env
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 // what we pass for our method
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
@@ -78,8 +89,6 @@ app.use(routes);
 // app.get('/register', checkNotAuthenticated, (req,res) => {
 //     res.render('register.ejs')
 // })
-               
-
 
 // app.delete('/logout', function(req, res, next) {
 //     req.logout(function(err) {
@@ -89,23 +98,7 @@ app.use(routes);
 //   });
 
 
-// function checkAuthenticated(req, res, next) {
-//     if (req.isAuthenticated()) {
-//         return next()
-//     }
-
-//     res.redirect('/login')
-// }
-
-// function checkNotAuthenticated (req, res, next) {
-//     if (req.isAuthenticated()) {
-//        return res.redirect('/')
-//     }
-
-//     next()
-// }
-
 
 app.listen(PORT, () => {
-    console.log(`listening on port:${PORT}`);
-})
+  console.log(`listening on port:${PORT}`);
+});
