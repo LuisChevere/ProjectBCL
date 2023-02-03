14c89b3f42a83612b9fa03b68955c9e16bdd63c1
Bonee94 <bonee94@yahoo.com>
2023-02-02 23:39:46 -0600

    Added routes for login and logout api calls. Added rendering variables for being logged in.

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,83 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const { serializeUser, initialize } = require("passport");
+const passport = require("passport");
+const { User, Agent } = require("../../models");
+const initializePassport = require('../../passport-config')
+
+router.post("/register", async (req, res) => {
+  // creating hashed password
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const agentData = await Agent.findAll();
+    const agents = agentData.map((agent) => agent.get({ plain: true }));
+    const agentNum = Math.floor(Math.random() * agents.length) + 1;
+
+    await User.create({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      is_agent: false,
+      email: req.body.email,
+      password: hashedPassword,
+      agent_id: agentNum,
+    });
+
+    const userData = await User.findOne({
+      where: {email: req.body.email}
+    })
+
+    const user = userData.get({ plain: true });
+
+    res.status(200).json("OK")
+
+    //res.status(200).render('agents');
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    const user = await userData.get({plain: true});
+
+    if (!user) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+    const validPassword = bcrypt.compare(req.body.password, user.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.logged_in = true;
+
+      res.json({ message: "You are now logged in!" });
+    });
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
+module.exports = router;
