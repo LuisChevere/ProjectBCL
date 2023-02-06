de50edf8718e5a4c6397a6023585d2a6d137d8ed
Bonee94 <bonee94@yahoo.com>
2023-02-06 04:01:10 -0600

    Testing heroku .env

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,8 @@
 const Sequelize = require('sequelize');
 
-
 //require('dotenv').config();
 
 // create connection to our db
-const sequelize = (new Sequelize(process.env.JAWSDB_URL), {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+const sequelize = Sequelize.createConnection(process.env.JAWSDB_URL);
 
 module.exports = sequelize;
